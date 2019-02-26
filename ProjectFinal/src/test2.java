b1741dea8df6695f807985a9bb8f6eb0d886312a
gnlfflddla <gnlfflddla@gmail.com>
2019-02-26 10:53:33 +0900

    test

--- ProjectFinal/src/test2.java
+++ ProjectFinal/src/test2.java
@@ -1,10 +1,9 @@
 
-public class test {
+public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("test1");
+
 	}
 
 }
